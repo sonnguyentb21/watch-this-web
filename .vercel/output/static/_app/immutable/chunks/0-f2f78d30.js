cd4dedd29d992d1fddbe4e44586bfd60e6a2237f
sondev22 <sonnguyentb21@gmail.com>
2023-04-27 09:35:48 +0700

    remove vercel folder

--- .vercel/output/static/_app/immutable/chunks/0-f2f78d30.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/_layout.svelte-82482edb.js";export{t as component};
--- .vercel/output/static/_app/immutable/chunks/2-8dcfd2dd.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/_page.svelte-1ca36a0a.js";export{t as component};
